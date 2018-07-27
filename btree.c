010bce041d1bdf3892a4215d16b6b1b92e19d44f
SZABO Gergely <szg@subogero.com>
2018-07-27 19:23:44 +0200

    btree AVL balance segfault

--- btree.c
+++ btree.c
@@ -8,15 +8,16 @@
 #define N 100
 
 struct t_bt {
-	struct t_bt *branch1;
-	struct t_bt *branch2;
+	struct t_bt *left;
+	struct t_bt *right;
 	int leaf;
 	char has_leaf;
+	char height;
 };
 
 void print_leaf(int leaf, int level)
 {
-	printf("%5d on level %2d\n", leaf, level);
+	printf("%5d height %2d\n", leaf, level);
 }
 
 /*
@@ -24,34 +25,94 @@ void print_leaf(int leaf, int level)
  */
 struct t_bt *create_bt(void) {
 	struct t_bt *new = malloc(sizeof(struct t_bt));
-	new->branch1 = NULL;
-	new->branch2 = NULL;
+	new->left = NULL;
+	new->right = NULL;
 	new->has_leaf = 0;
+	new->height = -1;
 	return new;
+}
+
+/*
+ * Calculate height of tree node
+ */
+void calc_height(struct t_bt *bt)
+{
+	int height = -1;
+	if (bt->left != NULL && bt->left->height > height)
+		height = bt->left->height;
+	if (bt->right != NULL && bt->right->height > height)
+		height = bt->right->height;
+	bt->height = height + 1;
+}
+
+/*
+ * Left rotate tree node - return left as root
+ */
+struct t_bt *left_rotate(struct t_bt *bt)
+{
+	struct t_bt *root = bt->left;
+
+	bt->left = root->right;
+	calc_height(bt);
+
+	root->right = bt;
+	calc_height(root);
+
+	return root;
+}
+
+/*
+ * Right rotate tree node - return right as root
+ */
+struct t_bt *right_rotate(struct t_bt *bt)
+{
+	struct t_bt *root = bt->right;
+
+	bt->right = root->left;
+	calc_height(bt);
+
+	root->left = bt;
+	calc_height(root);
+
+	return root;
 }
 
 /*
  * Insert int into btree, lower value to 1st branch
  */
-void insert_bt(struct t_bt *bt, int leaf, int level)
+struct t_bt *insert_bt(struct t_bt *bt, int leaf, int level)
 {
 	level++;
 	if (!bt->has_leaf) {
 		level--;
 		bt->leaf = leaf;
 		bt->has_leaf = 1;
-		print_leaf(bt->leaf, level);
 	} else if (leaf < bt->leaf) {
-		if (bt->branch1 == NULL) {
-			bt->branch1 = create_bt();
+		if (bt->left == NULL)
+			bt->left = create_bt();
+		bt->left = insert_bt(bt->left, leaf, level);
+		/* Balance if necessary */
+		char r_height = bt->right ? bt->right->height : -1;
+		if (bt->left->height >= r_height + 2) {
+			if (leaf >= bt->left->leaf)
+				bt->left = right_rotate(bt->left);
+			return left_rotate(bt);
 		}
-		insert_bt(bt->branch1, leaf, level);
 	} else {
-		if (bt->branch2 == NULL) {
-			bt->branch2 = create_bt();
+		if (bt->right == NULL)
+			bt->right = create_bt();
+		bt->right = insert_bt(bt->right, leaf, level);
+		/* Balance if necessary */
+		char l_height = bt->left ? bt->left->height : -1;
+		if (bt->right->height >= l_height + 2) {
+			if (leaf >= bt->right->leaf)
+				bt->right = left_rotate(bt->right);
+			return right_rotate(bt);
 		}
-		insert_bt(bt->branch2, leaf, level);
 	}
+
+	calc_height(bt);
+	return bt;
 }
 
 /*
@@ -62,9 +123,9 @@ void dtravers_bt(struct t_bt *bt, int level, void (*fn)(int, int))
 {
 	if (bt == NULL)
 		return;
-	dtravers_bt(bt->branch1, level+1, fn);
+	dtravers_bt(bt->left, level+1, fn);
 	(*fn)(bt->leaf, level);
-	dtravers_bt(bt->branch2, level+1, fn);
+	dtravers_bt(bt->right, level+1, fn);
 }
 
 int main(int argc, char *argv[])
